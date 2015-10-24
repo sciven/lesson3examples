8d30887ed3b7195241e138b1a381ce4f61aaf4b0
sciven <zorin@scienceventure.ca>
2015-10-24 13:10:58 -0700

    added int pinNumber = 12;

--- makeBlink/makeBlinkUNO.ino
+++ makeBlink/makeBlinkUNO.ino
@@ -1,4 +1,6 @@
 int counter = 0;
+int pinNumber = 8;
+
 void setup() {
 
   Serial.begin(9600);
@@ -6,8 +8,8 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
  
-  // initialize digital pin 12 as an output.
-  pinMode(12, OUTPUT);
+  // initialize digital pin  as an output.
+  pinMode(pinNumber, OUTPUT);
 
 }
 
@@ -24,9 +26,9 @@ void loop() {
 
 void make_blink(){
  
-      digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+      digitalWrite(pinNumber, HIGH);   // turn the LED on (HIGH is the voltage level)
       delay(1000);              // wait for a second
-      digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+      digitalWrite(pinNumber, LOW);    // turn the LED off by making the voltage LOW
       delay(1000);              // wait for a second
 
 }
